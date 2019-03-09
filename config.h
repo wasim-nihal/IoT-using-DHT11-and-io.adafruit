02fa4090bcac0a1b0a485586ffad3c877fe9bf67
wasim-nihal <38865967+wasim-nihal@users.noreply.github.com>
2019-03-09 21:27:56 +0530

    Rename config.h.ino to config.h

---
+++ config.h
@@ -0,0 +1,14 @@
+
+#define IO_USERNAME    "wasimnihal"
+#define IO_KEY         "6f6d5e63f4bf4a04babf021b51d4d7da"
+
+
+#define WIFI_SSID       "Private Network"
+#define WIFI_PASS       "mynameissyedwasimnihalandiusele2phone"
+
+
+#include "AdafruitIO_WiFi.h"
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+
+
+
